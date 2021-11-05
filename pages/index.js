1f261b5244b15bdd2fcbce1958ab84cd252aa96a
Thomas Boittin <tboittin@gmail.com>
2021-11-05 10:41:35 +0100

    head infos

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,68 @@
-import Contact from "../components/elements/Contact";
-import Home from "../components/elements/Home";
-import Portfolio from "../components/elements/Portfolio";
-import Skills from "../components/elements/Skills";
-import Timeline from "../components/elements/Timeline";
+import Head from "next/head"
+
+import Contact from "../components/elements/Contact"
+import Home from "../components/elements/Home"
+import Portfolio from "../components/elements/Portfolio"
+import Skills from "../components/elements/Skills"
+import Timeline from "../components/elements/Timeline"
 // import News from "../components/elements/News";
 // import Price from "../components/elements/Price";
-import Footer from "../components/layout/Footer";
-import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer"
+import Header from "../components/layout/Header"
 
 export default function Index() {
-    return (
-        <div id="opened">
-            <div
-                className="kura_tm_all_wrap"
-                data-color="orange"
-            >
-                <div id="preloader">
-                    <div className="loader_line"></div>
-                </div>
+  const pageTitle = "Thomas Boittin - Front End Developer"
+  const description =
+    "Thomas Boittin - Portfolio. French developer enthusiast with graphic skills."
+    const currentURL= "https://kura-website.vercel.app/"
+    const previewImage = "img/hero/1.jpg"
+  return (
+    <div id="opened">
+      <div className="kura_tm_all_wrap" data-color="orange">
+        <div id="preloader">
+          <div className="loader_line"></div>
+        </div>
 
-                <Header />
+        <Head>
+          <title>{pageTitle}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta name="description" content={description} />
+          <meta property="og:title" content={pageTitle} key="ogtitle" />
+          <meta property="og:description" content={description} key="ogdesc" />
+          <meta property="og:url" content={currentURL} key="ogurl" />
+          <meta property="og:image" content={previewImage} key="ogimage" />
+          <meta property="og:site_name" content={pageTitle} key="ogsitename" />
+        </Head>
 
-                <Home />
+        <Header />
 
-                <Portfolio />
+        <Home />
 
-                <Skills />
+        <Portfolio />
 
-                <Timeline />
+        <Skills />
 
-                {/* <Price />
+        <Timeline />
+
+        {/* <Price />
 
                 <News /> */}
 
-                <Contact />
+        <Contact />
 
-                <Footer />
+        <Footer />
 
-                <audio id="audio1">
-                    <source src="/audio/1.mp3" />
-                </audio>
-                <audio id="audio2">
-                    <source src="/audio/2.mp3" />
-                </audio>
+        <audio id="audio1">
+          <source src="/audio/1.mp3" />
+        </audio>
+        <audio id="audio2">
+          <source src="/audio/2.mp3" />
+        </audio>
 
-                <div className="mouse-cursor cursor-outer"></div>
-                <div className="mouse-cursor cursor-inner"></div>
-            </div>
-        </div>
-    );
+        <div className="mouse-cursor cursor-outer"></div>
+        <div className="mouse-cursor cursor-inner"></div>
+      </div>
+    </div>
+  )
 }
