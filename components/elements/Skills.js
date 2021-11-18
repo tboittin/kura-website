a6517b1cd36350266b7d981a4fd7e79cb8aacd03
Thomas Boittin <tboittin@gmail.com>
2021-11-18 11:35:36 +0100

    skills i18n

--- components/elements/Skills.js
+++ components/elements/Skills.js
@@ -1,70 +1,69 @@
-import dynamic from "next/dynamic";
-import React, { useEffect } from "react";
+import dynamic from "next/dynamic"
+import React, { useEffect } from "react"
 
-import Pill from '../design/pill'
+import Pill from "../design/pill"
 
-import SKILLS from '../../data/SKILLS.json'
+import SKILLS from "../../data/SKILLS.json"
 import IMG_CREDIT from "../../data/IMG_CREDIT.json"
 
-
 const ParallaxVideo = dynamic(import("./Parallax"), {
-    ssr: false,
-});
+  ssr: false,
+})
 
+const Skills = ({ languageContent, locale }) => {
+  useEffect(() => {
+    const boxes = document.querySelectorAll(".bar")
 
-const Skills = () => {
-    useEffect(() => {
-        const boxes = document.querySelectorAll(".bar");
+    window.addEventListener("scroll", checkBoxes)
 
-        window.addEventListener("scroll", checkBoxes);
+    checkBoxes()
 
-        checkBoxes();
+    function checkBoxes() {
+      const triggerBottom = (window.innerHeight / 5) * 5
 
-        function checkBoxes() {
-            const triggerBottom = (window.innerHeight / 5) * 5;
+      boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
 
-            boxes.forEach((box) => {
-                const boxTop = box.getBoundingClientRect().top;
-
-                if (boxTop < triggerBottom) {
-                    box.classList.add("open");
-                } else {
-                    box.classList.remove("open");
-                }
-            });
+        if (boxTop < triggerBottom) {
+          box.classList.add("open")
+        } else {
+          box.classList.remove("open")
         }
-    });
+      })
+    }
+  })
 
-    return (
-        <div>
-            <div className="kura_tm_section" id="skills">
-                <div className="kura_tm_skills">
-                    <div className="container">
-                        <div className="skills_inner">
-                            <div className="left">
-                                <div className="kura_tm_main_title light">
-                                    <span>Skills</span>
-                                    <h3>Programming Skills</h3>
-                                </div>
-                                <div
-                                    className="text wow fadeInUp"
-                                    data-wow-duration=".7s"
-                                >
-                                    <p>
-                                        Here are some tools that I use to create web applications.
-                                    </p>
-                                </div>
-                                {SKILLS && 
-                                    <div className="skills-pills">
-                                        <h4>Languages</h4>
-                                            {SKILLS.languages.map(skill => <Pill text={skill} key={skill}/>)}
-                                        <h4>Tools</h4>
-                                            {SKILLS.tools.map(skill => <Pill text={skill} key={skill}/>)}
-                                        <h4>Soft Skills</h4>
-                                            {SKILLS.softSkills.map(skill => <Pill text={skill} key={skill}/>)}
-                                    </div>
-                                }
-                                {/* <div
+  return (
+    <div>
+      <div className="kura_tm_section" id="skills">
+        <div className="kura_tm_skills">
+          <div className="container">
+            <div className="skills_inner">
+              <div className="left">
+                <div className="kura_tm_main_title light">
+                  <span>Skills</span>
+                  <h3>{languageContent.skillTitle}</h3>
+                </div>
+                <div className="text wow fadeInUp" data-wow-duration=".7s">
+                  <p>{languageContent.skillSubtitle}</p>
+                </div>
+                {SKILLS[locale] && (
+                  <div className="skills-pills">
+                    <h4>Languages</h4>
+                    {SKILLS[locale].languages.map(skill => (
+                      <Pill text={skill} key={skill} />
+                    ))}
+                    <h4>Tools</h4>
+                    {SKILLS[locale].tools.map(skill => (
+                      <Pill text={skill} key={skill} />
+                    ))}
+                    <h4>Soft Skills</h4>
+                    {SKILLS[locale].softSkills.map(skill => (
+                      <Pill text={skill} key={skill} />
+                    ))}
+                  </div>
+                )}
+                {/* <div
                                     className="dodo_progress wow fadeInUp"
                                     data-wow-duration=".7s"
                                     data-wow-delay=".2s"
@@ -116,29 +115,26 @@ const Skills = () => {
                                         </div>
                                     </div>
                                 </div> */}
-
-                            </div>
-                            <div className="right">
-                                {/* <ParallaxVideo /> */}
-                                <div
-                                    className="my_image jarallax"
-                                    data-speed="0"
-                                    title={IMG_CREDIT["ming-shan-vertical"]}
-                                    style={{
-                                        backgroundImage:
-                                            "url(/img/ming-shan-vertical.jpg)",
-                                        backgroundAttachment:
-                                            "fixed",
-                                        backgroundPositionX: "450px"
-                                    }}
-                                ></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
+              <div className="right">
+                {/* <ParallaxVideo /> */}
+                <div
+                  className="my_image jarallax"
+                  data-speed="0"
+                  title={IMG_CREDIT["ming-shan-vertical"]}
+                  style={{
+                    backgroundImage: "url(/img/ming-shan-vertical.jpg)",
+                    backgroundAttachment: "fixed",
+                    backgroundPositionX: "450px",
+                  }}
+                ></div>
+              </div>
             </div>
+          </div>
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default Skills;
+export default Skills
